8d81a5801af062a8613b85d252cc46f280804839
Alex Baldwin <alexbaldwin@gmail.com>
2017-03-15 12:28:24 -0700

    Resetting the icon import

--- build.js
+++ build.js
@@ -1,51 +1,12 @@
-
 // Node script for reading SVG icons and creating a paths JSON file
-
 const fs = require('fs')
 const path = require('path')
 const cheerio = require('cheerio')
-const geomiconsKeys = require('./geomicons-keys')
-
 
 // Envoy Icons
 const envoyDir = path.join(__dirname, 'icons')
 const envoyFiles = fs.readdirSync(envoyDir)
   .filter(f => /\.svg$/.test(f))
-
-// Simple Icons
-const simpleDir = path.join(__dirname, 'node_modules', 'simple-icons#gh-pages', 'icons')
-const simpleFiles = fs.readdirSync(simpleDir)
-  .filter(f => /\.svg$/.test(f))
-
-const mdKeys = [
-  'action',
-  'alert',
-  'av',
-  'communication',
-  'content',
-  'device',
-  'editor',
-  'file',
-  'hardware',
-  'image',
-  'maps',
-  'navigation',
-  'notification',
-  'places',
-  'social',
-  'toggle',
-]
-
-const mdDir = mdKeys.reduce((a, key) => {
-  a[key] = path.join(__dirname, 'node_modules', 'material-design-icons', key, 'svg', 'production')
-  return a
-}, {})
-
-const mdFilter = f => /24px\.svg$/.test(f)
-const mdFiles = mdKeys.reduce((a, key) => {
-  a[key] = fs.readdirSync(mdDir[key]).filter(mdFilter)
-  return a
-}, {})
 
 const getPaths = $ => {
   const iterable = $('path')
@@ -63,8 +24,8 @@ const createPaths = dir => filenames => {
     const root = cheerio.load(svg)
     const values = getPaths(root)
     const key = f.replace(/\.svg$/, '')
-      .replace(/^ic_/, '')
-      .replace(/_24px$/, '')
+      .replace('garaje_slate-tt-48_', '')
+      .replace('standard_slate-tt-48_', '')
 
     return {
       key,
@@ -78,8 +39,6 @@ const createPaths = dir => filenames => {
 
   return paths
 }
-
-
 
 const envoyPaths = createPaths(envoyDir)(envoyFiles)
 const envoyJs = `module.exports = ${JSON.stringify(envoyPaths)}`
